c73d8864436aff2f89c116a25c6fa552258fa54f
Dmitri Samotessov <dimasamo96@gmail.com>
2019-02-20 02:16:57 +0000

    Automatically backed up by Learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,10 +1,10 @@
 class Pokemon
   attr_accessor :id, :name, :type, :db
-  def initialize(id, name, type, db)
-    @id = :id
-    @name = :name
-    @type = :type
-    @db = :db
+  def initialize(id:, name:, type:, db:)
+    # @id = :id
+    # @name = :name
+    # @type = :type
+    # @db = :db
   end
 
   def self.save(name,type,db)
@@ -13,7 +13,7 @@ class Pokemon
 
   def self.find(id, db)
     pokemon = db.execute("SELECT id, name, type FROM pokemon WHERE id = ?", id).flatten
-    Pokemon.new(pokemon[0],pokemon[1],pokemon[2], db)
+    Pokemon.new(pokemon[0], pokemon[1], pokemon[2], db)
   end
 
 end
