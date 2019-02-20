9f9e7b1b9e7e8677ace05b05f61b89ced4eb9dbd
Dmitri Samotessov <dimasamo96@gmail.com>
2019-02-20 02:22:47 +0000

    Automatically backed up by Learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -12,7 +12,7 @@ class Pokemon
     pokemon = db.execute("SELECT id, name, type FROM pokemon WHERE id = ?", id).flatten
     new_pokemon=Pokemon.new(pokemon)
     self.save(new_pokemon)
-    new_pokemon
+    new_pokemon.name
   end
 
 end
